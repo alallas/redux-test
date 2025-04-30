299ed905d7a6dbab0a329259c0848d2352912434
alallas <798799155@qq.com>
2025-05-01 00:02:28 +0800

    1

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            }
+          },
+          {
+            loader: 'source-map-loader',
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './public/index.html',
+      chunks: ['main']
+    })
+  ],
+  devServer: {
+    host: '127.0.0.1',
+    port: '8081',
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '.test'           // 允许所有 .test 结尾的域名
+    ],
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  }
+}
